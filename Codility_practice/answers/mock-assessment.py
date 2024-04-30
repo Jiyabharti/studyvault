477767d53d33f9fb9d600ce0b039b3f4073f12f8
Jiya Bharti <Jiyabharti@oulook.com>
2024-05-01 00:15:02 +0100

    re-organised directories

---
+++ Codility_practice/answers/mock-assessment.py
@@ -0,0 +1,20 @@
+
+
+# TASK 1
+
+def solution(S):
+    # Implement your solution here
+    S = S.replace("minus","-")
+    S = S.replace("plus", "+")
+    return S
+# how do i test this?
+
+# TASK 2
+
+def solution(A):
+    # Implement your solution here
+    A = max(A) - min(A)
+    return A
+
+# TASK 3
+
