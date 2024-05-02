cc4594e210f41bfdd1c65080031654545c3b842d
Jiya Bharti <Jiyabharti@oulook.com>
2024-05-03 00:09:55 +0100

    started sentence smash question

---
+++ Questions_practice/common_questions/leap_year.py
@@ -0,0 +1,8 @@
+
+#  A leap year is a year with an additional day
+# instead of 365 days, there is 366 days, it happens every 4 years
+
+"""Some psuedo code"""
+#  there thus, must be some conditions to a leap year
+# if number is divisible by 400 its a leap year
+
---
+++ Questions_practice/sentence_smash.py
@@ -0,0 +1,15 @@
+# found on codewars
+
+
+
+#
+emotions = ["happy", "sad", "angry", "love"]
+# def smash(words):
+#     enumerate(words)
+#     return smash(words)
+#
+# smash(emotions)
+
+def smash(emotions):
+    for word in emotions:
+        return emotions
